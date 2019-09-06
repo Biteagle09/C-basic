7ef0abe02c4cfa6963f4c96fd4e52ac87eb2b10f
Biteagle09 <2894544291@qq.com>
2019-09-07 00:27:53 +0800

    Memory alignment and union

--- C-basic/structTest.cpp
+++ C-basic/structTest.cpp
@@ -14,7 +14,7 @@ typedef struct DT {
 	unsigned short second;
 }DateTime;
 
-
+ 
 //定义一个结构体
 struct  Person{
 	char name[30];
@@ -25,6 +25,35 @@ struct  Person{
 	struct DT birthday;
 };
 
+struct Node{
+	int data;
+	struct Node* next;
+};
+
+struct Test1 {
+	double d;
+	int a;
+	short s;
+};
+
+struct Test3 {
+	double d;
+	short s;
+	int a;
+};
+
+struct Test2 {
+	short s;
+	double d;
+	int a;
+};
+
+
+struct Test4 {
+	char c;
+	int i;
+	short s;
+};
 
 //结构体测试
 struct A {
@@ -33,19 +62,48 @@ struct A {
 };
 
 
+//共同体
+union TestU {
+	char i;
+	short j;
+};
+
+
+
 int main() {
 
 	struct Person p1 = { "王坚","男",43,176,66.783456f,{1982,12,31,18,23,51} };
 	struct Person p2 = {"张小龙","男",54,177,69.23456f};
 	DT t1 = {2019,11,5};
 
-	showTime(&t1);
+	//showTime(&t1);
 
 	//p1.weight = 1346;
 	//say(p1);
 	//printf_s("****************************************\n");
 	//say(p2);
 
+
+	
+	struct Node* n1 = (struct Node*)malloc(sizeof(struct Node));
+	struct Node* n2 = (struct Node*)malloc(sizeof(struct Node));
+	struct Node* n3 = (struct Node*)malloc(sizeof(struct Node));
+	n1->data = 1;
+	n2->data = 2;
+	n3->data = 3;
+	n1->next = n2;
+	n2->next = n3;
+
+	//printf_s("Test1==%d\n", sizeof(Test1));
+	//printf_s(" Test2==%d\n", sizeof(Test2));
+	//printf_s("Test3==%d\n", sizeof(Test3));
+	//printf_s("Test4==%d\n", sizeof(Test4));
+
+	struct Node arr[3] = {*n1,*n2,*n3};
+
+	union TestU  tu = { 996 };
+	printf_s("TestU.i==%d\n", tu.i);
+	printf_s("TestU.j==%d\n", tu.j);
 	return 0;
 }
 
@@ -62,3 +120,9 @@ void say(struct Person p) {
 void showTime(struct DT *dte) {
 	printf_s("%04d-%02d-%02d  %02d:%02d:%02d\n", dte->year, dte->month, dte->day, dte->hour, dte->minute, dte->second);
 }
+
+
+
+
+
+
